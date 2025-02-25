796d741e75719a2c0429d4a45c9ad4621826f813
Oleksandr Grechaniuk <oleksandr.grechaniuk@playtech.com>
2025-02-25 15:14:08 +0200

    Added parameters for fetch news

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { fetchNews } from "./services/NewsApiService";
+
+fetchNews()
+    .then((response) => {
+        console.log('response', response);
+    })
+    .catch((error) => {
+        console.log('Could not fetch', error);
+    });
+
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
--- src/services/NewsApiService.ts
+++ src/services/NewsApiService.ts
@@ -1,43 +1,14 @@
-const API_BASE = 'https://newsapi.org/v2/';
+const API_BASE = 'https://newsapi.org/v2/everything/';
+const API_KEY = 'apiKey=dc71297702b14b8aa2739c646a6a118b';
 
 
-const fetchNews = async (url: string) => {
-    const response = await fetch(url);
+export const fetchNews = async (query: string = 'q=tesla', sortBy: string = 'sortBy=published-at', language: string = 'language=en') => {
+    const response = await fetch(`${API_BASE}?${query}&${sortBy}&${language}&${API_KEY}`);
 
     if (!response.ok) {
-        throw new Error(`${response.statusText} Could not fetch ${url}`);
+        throw new Error(`${response.statusText} Could not fetch ${API_BASE}&${query}&${sortBy}&${language}${API_KEY}`);
     }
 
     const json = await response.json();
     return json;
 };
-
-fetchNews('https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&apiKey=dc71297702b14b8aa2739c646a6a118b')
-    .then((response) => {
-        console.log('response', response);
-    })
-    .catch((error) => {
-        console.log('Could not fetch', error);
-    });
-
-/*
-fetch('https://newsapi.org/v2/everything?q=tesla&from=2025-01-24&sortBy=publishedAt&apiKey=dc71297702b14b8aa2739c646a6a118b')
-    .then(response => response.json())
-    .then((json) => {
-        console.log(json);
-    });
-
-fetch('https://newsapi.org/v2/everything?q=tesla&sortby=published-at&language=en&apiKey=241320bb4ba14d01b463cbf728dde5a0')
-    .then(response => response.json())
-    .then((json) => {
-        console.log(json);
-    });
-
-fetch('https://newsapi.org/v2/everything?q=tesla&from=2025-01-24&sortBy=publishedAt&language=de&apiKey=dc71297702b14b8aa2739c646a6a118b')
-    .then(response => response.json())
-    .then((json) => {
-        console.log(json);
-    });
-*/
-
-export {}
