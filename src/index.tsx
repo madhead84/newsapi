4d6a0e4d00e1dd60db6bc12c220f1d97af51dd48
Oleksandr Grechaniuk <oleksandr.grechaniuk@playtech.com>
2025-02-25 12:19:39 +0200

    Start working on services

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-fetch('https://newsapi.org/v2/everything?q=tesla&from=2025-01-24&sortBy=publishedAt&apiKey=dc71297702b14b8aa2739c646a6a118b')
-    .then(response => response.json())
-    .then((json) => {
-        console.log(json);
-    });
-
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
+
+
 root.render(
     <React.StrictMode>
         <App/>
